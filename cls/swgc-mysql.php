9763f7b9843201783c23acfbfa1250c6342053d9
Mario Ernesto Basurto Medrano <mario@d80.mx>
2020-06-22 18:01:31 -0500

    Commit Inicial
    Primer Commit de Aplicativo (producción)

---
+++ cls/swgc-mysql.php
@@ -0,0 +1,8 @@
+<?php
+
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+//error_reporting(E_ALL);
+$conexion = mysql_connect("localhost","sdiba691_root","B@surto91");
+mysql_select_db("sdiba691_gep");
+?>
